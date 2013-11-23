b39b98a3713d9a765dd63f11b4a0fa8d6a73184a
David Cox <losttime.shuffle@gmail.com>
2013-11-23 04:35:04 -0700

    Adds time information to displayed task list
    If the task is ongoing, the start time is appended to the end of the
    task name. If the task has already ended, the task duration is appended
    to the end of the task name.

--- Task Timer/src/main/java/com/dconstructing/android/tasktimer/objects/Task.java
+++ Task Timer/src/main/java/com/dconstructing/android/tasktimer/objects/Task.java
@@ -2,6 +2,9 @@ package com.dconstructing.android.tasktimer.objects;
 
 import android.text.format.Time;
 
+import org.joda.time.Duration;
+import org.joda.time.Interval;
+
 public class Task {
 
 	public Time startTime;
@@ -18,6 +21,36 @@ public class Task {
 	}
 
 	public String toString() {
-		return this.label;
+		String output = this.label + " -";
+		if (this.endTime != null) {
+			long startMilli = this.startTime.toMillis(false);
+			long endMilli = this.endTime.toMillis(false);
+			Interval interval = new Interval(startMilli, endMilli);
+			Duration duration = interval.toDuration();
+			long days = duration.getStandardDays();
+			if (days > 0) {
+				output += " " + days + "d";
+				duration = duration.minus(days*86400000);
+			}
+			long hours = duration.getStandardHours();
+			if (hours > 0) {
+				output += " " + hours + "h";
+				duration = duration.minus(hours*3600000);
+			}
+			long minutes = duration.getStandardMinutes();
+			if (minutes > 0) {
+				output += " " + minutes + "m";
+				duration = duration.minus(minutes*60000);
+			}
+			long seconds = duration.getStandardSeconds();
+			if (seconds > 0) {
+				output += " " + seconds + "s";
+			}
+		} else {
+			this.startTime.switchTimezone(Time.getCurrentTimezone());
+			output += " " + this.startTime.format("%l:%M%p"); // 3:14pm - http://linux.die.net/man/3/strftime
+			//output += " " + this.startTime.format("%b %e @ %l:%M%p"); // Jan 3 @ 3:14pm
+		}
+		return output;
 	}
 }
